068d7cada379b20c52b6da1b3c2165e9ea90d93e
jvitormoraes43-ai <j.vitormoraes43@gmail.com>
2026-08-05 22:28:18 -0300

    Add files via upload

---
+++ aula 2 media.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	
+	int a, b, c;
+	float resultado;
+	
+	a = 10;
+	b = 10;
+	c = 10;
+	
+	resultado = (a+b+c)/3;
+		
+	printf("resultado %f", resultado);
+	return 0;
+}
