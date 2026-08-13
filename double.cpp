f6ba9a155c9178138479d522d550255b43693af5
jvitormoraes43-ai <j.vitormoraes43@gmail.com>
2026-08-12 22:30:46 -0300

    Add files via upload

---
+++ double.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc, char *argv[]){
+	
+	/*int primeiro, segundo, aux;
+	
+	printf("Escreva o primeiro valor: ");
+	scanf("%d", &primeiro);
+	
+	printf("Escreva o segundo valor: ");
+	scanf("%d", &segundo);
+	
+	aux = primeiro;
+	primeiro = segundo;
+	segundo = aux;
+	
+	printf("%d \n %d", segundo, primeiro);
+	*/
+	
+double primeiro, segundo;
+
+	printf("Escreva o valor: ");
+	scanf("%lf", &primeiro);
+	
+	printf ("%e \n", primeiro );
+	
+	return 0;
+}
