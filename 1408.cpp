8dba79f1cb76e51e7b2f63135388157a8bdd83e2
jvitormoraes43-ai <j.vitormoraes43@gmail.com>
2026-08-14 21:50:39 -0300

    Add files via upload

---
+++ 1408.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc, char *argv[]){
+//att 1	
+	/*int primeiro, segundo, aux;
+	
+	printf("Escreva o primeiro valor: ");
+	scanf("%d", &primeiro);
+	
+	printf("Escreva o segundo valor: ");
+	scanf("%d", &segundo);
+	
+	aux = primeiro;
+	primeiro = segundo;
+	segundo = aux;
+	
+	printf("%d \n %d", segundo, primeiro);
+	
+-----------------------------------------------------------------------------------*/
+//att 2 
+	/*double primeiro, segundo;
+
+	printf("Escreva o valor: ");
+	scanf("%lf", &primeiro);
+	
+	printf ("%e \n", primeiro );
+	
+------------------------------------------------------------------------------------	*/
+//att6 
+	/*int Idade, ano, mes, dia;
+	
+	printf("Escreva sua idade em dias: "); // %d significa Inteiro o numero
+	scanf("%d", &Idade);
+	
+	ano = Idade/365;
+	mes = Idade/30;
+	dia = Idade/1;
+
+	printf("%danos %dmeses  %ddias", ano, mes, dia);
+-------------------------------------------------------------------------------------*/
+//att 7 
+/*	double V, R, pi;
+	
+	printf("De o valor do raio para calcular a esfera: ");
+	scanf("%lf", &R);
+	
+	pi = 3.14;
+	V = (4.0/3*(pi*(R*R*R)));
+	
+	printf("Volume da massa e = %f", V);
+--------------------------------------------------------------------------------------*/
+	return 0;
+}
